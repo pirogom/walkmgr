afe878bbb31d3ac0bad3ede6ac8866b2b567d40a
Pirogom <mop.pirogom@gmail.com>
2022-03-25 18:31:05 +0900

    ;;;

--- walkmgr.go
+++ walkmgr.go
@@ -159,13 +159,6 @@ func NewAds(title string, width int, height int) *WalkUI {
 }
 
 /**
-*	SetTitle
-**/
-func (wm *WalkUI) SetTitle(title string) {
-	wm.window.SetTitle(title)
-}
-
-/**
 *	adsPosition
 **/
 func (wm *WalkUI) adsPosition() {
