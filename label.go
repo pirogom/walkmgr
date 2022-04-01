234b79d53f745d9aa73975a85c1d33d342d2d960
Pirogom <mop.pirogom@gmail.com>
2022-04-01 13:35:31 +0900

    remove MultiLineLabel

--- label.go
+++ label.go
@@ -2,35 +2,7 @@ package walkmgr
 
 import (
 	"github.com/pirogom/walk"
-	"github.com/pirogom/win"
 )
-
-/**
-*	MultiLineLabel
-**/
-func (wm *WalkUI) MultiLineLabel(text string, at ...AlignType) *walk.Label {
-	ne, _ := walk.NewLabelWithStyle(wm.Parent(), win.SS_EDITCONTROL)
-	ne.SetText(text)
-	ne.SetAlignment(walk.AlignHCenterVCenter)
-
-	if len(at) == 0 {
-		ne.SetTextAlignment(walk.AlignDefault)
-	} else {
-		switch at[0] {
-		case ALIGN_LEFT:
-			ne.SetTextAlignment(walk.AlignNear)
-		case ALIGN_CENTER:
-			ne.SetTextAlignment(walk.AlignCenter)
-		case ALIGN_RIGHT:
-			ne.SetTextAlignment(walk.AlignFar)
-		default:
-			ne.SetTextAlignment(walk.AlignDefault)
-		}
-	}
-
-	wm.Append(ne)
-	return ne
-}
 
 /**
 *	Label
