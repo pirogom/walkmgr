dda6fc9492e8fffa6086f8bfbce9a50afc173a70
Pirogom <mop.pirogom@gmail.com>
2022-12-02 12:11:39 +0900

    add statusbar(edit)

--- statusbar.go
+++ statusbar.go
@@ -3,20 +3,6 @@ package walkmgr
 import "github.com/pirogom/walk"
 
 /**
-*	StatusBarItems
-**/
-func (wm *WalkUI) StatusBarItems() *walk.StatusBarItemList {
-	return wm.Window().StatusBar().Items()
-}
-
-/**
-*	StatusBarVisible
-**/
-func (wm *WalkUI) StatusBarVisible(visible bool) {
-	wm.Window().StatusBar().SetVisible(visible)
-}
-
-/**
 *	AddStatusItem
 **/
 func (wm *WalkUI) AddStatusItem(text string, width int, icon *walk.Icon, clickedFn func()) *walk.StatusBarItem {
